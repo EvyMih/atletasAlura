6189950d9eba71643e9670c6b9f1cc9506bedf74
Evy Mih <evellynkoster@gmail.com>
2024-09-05 23:17:14 -0300

    PROJETO ALURA

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo: "Rafaela Silva",
+        descricao: "Rafaela Silva é um nome que brilha no cenário do judô mundial. Nascida e criada na comunidade da Cidade de Deus, no Rio de Janeiro, ela superou desafios e se tornou um dos maiores ícones do esporte brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Rafaela_Silva"
+    }
+];
